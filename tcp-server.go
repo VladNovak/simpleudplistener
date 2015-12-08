97971457387fb9a33ba4b271244fc3b2fea4a2d4
VladNovak <vladislav.novak@dunelm.org.uk>
2015-12-08 17:37:00 +0000

    Update tcp-server.go
    introducing "terminate" command

--- tcp-server.go
+++ tcp-server.go
@@ -12,7 +12,7 @@ func main() {
 
   if (len(args) > 1) && (args[1] == "udp") {
      fmt.Println("Launching server in udp mode...")
-     ln, _ := net.ResolveUDPAddr("udp",":8010")
+     ln, _ := net.ResolveUDPAddr("udp", "8010")
     
      /* Now listen at selected port */
      conn, _ := net.ListenUDP("udp", ln)
@@ -26,6 +26,8 @@ func main() {
         newmessage := strings.ToUpper(message[:len(message)-1])
         if newmessage == "STATUS" {
           conn.WriteTo([]byte("OK\n"),addr)
+        } else if newmessage = "TERMINATE" {
+          break
         } else {
           // send new string back to client
           conn.WriteTo([]byte(newmessage + "\n"),addr)
@@ -34,21 +36,24 @@ func main() {
 
   } else  {
       fmt.Println("Launching server...")
-     ln, _ := net.Listen("tcp", ":8010")
+     ln, _ := net.Listen("tcp", "8010")
 
      // accept connection on port
-     conn, _ := ln.Accept()
      for {
+      conn, _ := ln.Accept()
+      for {
        // will listen for message to process ending in newline (\n)
        message, _ := bufio.NewReader(conn).ReadString('\n')
        newmessage := strings.ToUpper(message[:len(message)-1])
        if newmessage == "STATUS" {
           conn.Write([]byte("OK\n"))
+       } else if newmessage = "TERMINATE" {
+          break
        } else {
           // send new string back to client
           conn.Write([]byte(newmessage + "\n"))
        }
      }
+     } 
   } 
-
 }
